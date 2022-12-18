3e889d76b59639d38d24dce68c787981534fdb6c
Nikhil Kumar <nikumar1206@gmail.com>
2022-12-17 23:56:49 -0500

    cards in grid format

--- frontend/src/components/feed.tsx
+++ frontend/src/components/feed.tsx
@@ -19,11 +19,12 @@ const Feed = () => {
 		});
 	}, []);
 	return (
-		<>
+		<div className="mt-5 grid grid-cols-3">
 			{posts.map((post) => {
 				return <PostCard post={post} key={post.id} />;
 			})}
-		</>
+		</div>
 	);
 };
+
 export default Feed;
--- frontend/src/components/postcard.tsx
+++ frontend/src/components/postcard.tsx
@@ -19,29 +19,23 @@ interface Post {
 
 const PostCard = ({ post }: { post: Post }) => {
 	return (
-		<Card className="w-96">
-			<CardHeader color="blue" className="relative h-56">
-				<img
-					src={post.imageUrl}
-					alt="img-blur-shadow"
-					className="h-full w-full"
-				/>
-			</CardHeader>
+		<Card className="w-96 m-0 mr-0">
+			<img
+				src={post.imageUrl}
+				alt="img-blur-shadow"
+				className="l object-contain overflow-hidden rounded-t-lg"
+			/>
 			<CardBody className="text-center">
 				<Typography variant="h5" className="mb-2">
 					{post.name}
 				</Typography>
-				<Typography>
-					The place is close to Barceloneta Beach and bus stop just 2 min by
-					walk and near to "Naviglio" where you can enjoy the main night life in
-					Barcelona.
-				</Typography>
+				<Typography>{post.caption}</Typography>
 			</CardBody>
 			<CardFooter divider className="flex items-center justify-between py-3">
-				<Typography variant="small">$899/night</Typography>
+				<Typography variant="small">{post.location}</Typography>
 				<Typography variant="small" color="gray" className="flex gap-1">
 					<i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-					Barcelona, Spain
+					{post.attendies.length}
 				</Typography>
 			</CardFooter>
 		</Card>
